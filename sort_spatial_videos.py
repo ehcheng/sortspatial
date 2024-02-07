a7efee97caafc42896fd080c8edb45b2920bf114
ehcheng <616681+ehcheng@users.noreply.github.com>
2024-02-07 15:31:15 -0800

    updated comments with underlying tooling requirements

--- sort_spatial_videos.py
+++ sort_spatial_videos.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. 
 
+# Relies on Mike Swanson's Spatial Video Tool executable: https://blog.mikeswanson.com/spatial
+
 import os
 import subprocess
 import shutil
